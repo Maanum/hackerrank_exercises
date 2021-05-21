1cfda561065c61bc1ff0d1d27db7f5f180cc050c
Maanum <kristofer.maanum@gmail.com>
2021-05-20 21:29:08 -0500

    Passing code

--- roads_and_libraries/roads_and_libraries.py
+++ roads_and_libraries/roads_and_libraries.py
@@ -17,32 +17,55 @@ import sys
 #  4. 2D_INTEGER_ARRAY cities
 #
 
+PROBLEM_INPUT = [
+    [3, 2, 1, [[1, 2], [3, 1], [2, 3]]],
+    [6, 2, 5, [[1, 3], [3, 4], [2, 4], [1, 2], [2, 3], [5, 6]]],
+    [5, 6, 1, [[1, 2], [1, 3], [1, 4]]],
+]
+
+
 def roadsAndLibraries(n, c_lib, c_road, cities):
-    # Write your code here
+    if c_lib < c_road:
+        return c_lib * n
+
+    networks = []
+
+    city_graph = {i : [j for j in cities if i in j] for i in range(1, n + 1)}
+    for key in city_graph:
+        city_graph[key] = set([item for sublist in city_graph[key] for item in sublist])
+
+    cities = list(city_graph.keys())
+
+    def get_network():
+        network = city_graph[cities[0]]
+        cities.pop(0)
+        def find_neighbors():
+            diff = network.intersection(set(cities))
+            if not diff:
+                return
+            else:
+                for city in diff:
+                    network.update(city_graph[city])
+                    cities.remove(city)
+                find_neighbors()
+        find_neighbors()
+        networks.append(network)
+        if len(cities) > 0:
+            get_network()
+
+    get_network()
+    cost_roads = c_road * (n - len(networks))
+    cost_libraries = c_lib * len(networks)
+
+    return cost_roads + cost_libraries
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = PROBLEM_INPUT
 
-    q = int(input().strip())
-
-    for q_itr in range(q):
-        first_multiple_input = input().rstrip().split()
-
-        n = int(first_multiple_input[0])
-
-        m = int(first_multiple_input[1])
-
-        c_lib = int(first_multiple_input[2])
-
-        c_road = int(first_multiple_input[3])
-
-        cities = []
-
-        for _ in range(m):
-            cities.append(list(map(int, input().rstrip().split())))
-
+    for q_itr in q:
+        n = int(q_itr[0])
+        c_lib = int(q_itr[1])
+        c_road = int(q_itr[2])
+        cities = q_itr[3]
         result = roadsAndLibraries(n, c_lib, c_road, cities)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+        print(f'result = {result}')
